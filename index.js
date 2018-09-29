d163d2cd0bdeb4b6d04b0ed0682a97fc68175a13
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-28 21:09:13 -0700

    periodic table structure, numbering, optional L/A expansion

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 "use strict";
 (function () {
 
+	const NORMAL_LAYOUT = [
+		[1, 1],
+		[2, 6],
+		[2, 6],
+		[3, 15],
+		[3, 15],
+		[3, 15, 14],
+		[3, 15, 14]
+	]
+
+	const EXPANDED_LAYOUT = [
+		[1, 1],
+		[2, 6],
+		[2, 6],
+		[3, 15],
+		[3, 15],
+		[17, 15],
+		[17, 15]
+	];
+
 	function $(selector) {
 		let result = document.querySelectorAll(selector);
 		return result.length > 1 ? result : result[0];
@@ -16,17 +36,43 @@
 		COLUMNS = 18;
 
 	window.addEventListener("load", function () {
-		generateHTML(ROWS, COLUMNS);
+		generateHTML($("table")[0], NORMAL_LAYOUT);
+		// generateHTML($("table")[1], EXPANDED_LAYOUT);
 	});
 
-	function generateHTML(rowCount, columnCount) {
-		let table = $("table");
+	function generateHTML(parentDOM, layout) {
+		// gets larger dimensions for iterator
+		let rowCount = layout.length;
+		let columnCount = 0;
+		for (let i = 0; i < layout.length; i++) {
+			let entry = layout[i];
+			columnCount = Math.max(columnCount, entry[0] + entry[1]);
+		}
+
+		console.log(rowCount + " rows of " + columnCount);
+
+		// iterates and creates DOM
+		let counter = 1;
 		for (let row = 0; row < rowCount; row++) {
 			let rowDOM = ce("tr");
+			let rowLayout = layout[row];
+			let addedInvisibleCount = false;
 			for (let col = 1; col <= columnCount; col++) {
-				rowDOM.appendChild(ce("td", row * columnCount + col));
+				let isFrontElement = col <= rowLayout[0];
+				let isBackElement = columnCount - col < rowLayout[1];
+				let cellDOM = ce("td");
+				// adds count for L/A series, etc (counted but can't see)
+				if (!isFrontElement && !addedInvisibleCount && rowLayout[2]) {
+					counter += rowLayout[2];
+					addedInvisibleCount = true;
+				}
+				if (isFrontElement || isBackElement) {
+					cellDOM.textContent = counter++;
+					cellDOM.className = "element";
+				}
+				rowDOM.appendChild(cellDOM);
 			}
-			table.appendChild(rowDOM);
+			parentDOM.appendChild(rowDOM);
 		}
 	}
 
